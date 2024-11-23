d28f57332975ebcadae21a6218b741d0733e8c79
Ahmad Noor <ahmadmalik00011@gmail.com>
2024-11-23 15:35:39 +0500

    new setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import ConvexClientProvider from "@/app/providers/ConvexClientProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,8 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
       </body>
     </html>
   );
--- convex/auth.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  providers: [
-    {
-      domain: "https://allowing-garfish-31.clerk.accounts.dev/",
-      applicationID: "convex",
-    },
-  ],
-};
